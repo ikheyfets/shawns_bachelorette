25e45d2c0f0a58d0c7f277e72cc4e1d02526ffa3
Igor Kheyfets <44104569+ikheyfets@users.noreply.github.com>
2025-10-05 13:37:19 -0700

    Update sketch.ino

--- arduino/sketch.ino
+++ arduino/sketch.ino
@@ -4,12 +4,13 @@ const int steeringInputPin = 2; // Connect the PWM signal from the receiver chan
 const int throttlingInputPin = 3; // Connect the PWM signal from the receiver channel 2 (throttling)
 
 // Defining motor A control inputs
-const int motorA1Pin = 5; //was 10 
+const int motorA1Pin = 7; //was 10 
 const int motorA2Pin = 6; //was 9
+const int motorAPwmPin = 5; // moved pins around because one of them needs to be PWM 
 
-// Defining motor B control inputs
-const int motorB1Pin = 9; //was 6
-const int motorB2Pin = 10; //was 5
+// // Defining motor B control inputs
+// const int motorB1Pin = 9; //was 6
+// const int motorB2Pin = 10; //was 5
 
 // PWM code source: https://lastminuteengineers.com/drv8833-arduino-tutorial/
 
@@ -20,10 +21,12 @@ void setup() {
 
   pinMode(motorA1Pin, OUTPUT);
   pinMode(motorA2Pin, OUTPUT);
-  pinMode(motorB1Pin, OUTPUT);
-  pinMode(motorB2Pin, OUTPUT);
+  pinMode(motorAPwmPin, OUTPUT);
+  
+  // pinMode(motorB1Pin, OUTPUT);
+  // pinMode(motorB2Pin, OUTPUT);
 
-  TurnOffMotors(motorA1Pin, motorA2Pin, motorB1Pin, motorB2Pin);
+  // TurnOffMotors(motorA1Pin, motorA2Pin, motorB1Pin, motorB2Pin);
 
   // Start serial communication at 9600 bps
   Serial.begin(9600);
@@ -43,74 +46,21 @@ void loop() {
     return; // Skip the remaining loop and move on to the next iteration
   }
 
- int ThrottlePWMOut = (((int) throttle - 50) * 5.1);
- set_motor_currents(ThrottlePWMOut,ThrottlePWMOut); //Math for PWM A
-  
-  // Ramp speed up.
-//  for (int i = 0; i < 11; i++) { //Initial condition = 0; While i<11, continue with loop; after every loop, add 1 to i
-//    spin_and_wait(25*i, 25*i, 500);
-//  }
-
-  // Log the data
-  // Serial.print("steer PWM = ");
-  // Serial.print(steer);
-  // Serial.println();
-
-  // Serial.print("throttle PWM = ");
-  // Serial.print(throttle);
-  // Serial.println();
-
-  // TODO: normalize the PWM data
-
-  // TODO: custom logic to drive two motors
-
-  // Wait for a short duration
-  delay(10);
-}
-
-/// Set the current on a motor channel using PWM and directional logic.
-///
-/// param pwm    PWM duty cycle ranging from -255 full reverse to 255 full forward
-/// param IN1_PIN  pin number xIN1 for the given channel
-/// param IN2_PIN  pin number xIN2 for the given channel
-void set_motor_pwm(int pwm, int IN1_PIN, int IN2_PIN)
-{
-  if (pwm < 0) {  // reverse speeds
-    analogWrite(IN1_PIN, -pwm);
-    digitalWrite(IN2_PIN, LOW);
-
-  } else { // stop or forward
-    digitalWrite(IN1_PIN, LOW);
-    analogWrite(IN2_PIN, pwm);
+  //  6-10 is throttle back, 10-14 is throttle forward
+  int speed = 255;
+  speed = speed * ((throttle - 10)/4);
+  Serial.println("Setting speed to:" + speed);
+  // set direction:
+  if (speed < 0) {
+      digitalWrite(motorA1Pin, LOW);
+      digitalWrite(motorA2Pin, HIGH);
+  } else {
+      digitalWrite(motorA1Pin, HIGH);
+      digitalWrite(motorA2Pin, LOW);
   }
+  //  set duty cycle:
+  analogWrite(motorAPwmPin, abs(speed));
 }
-
-/// Set the current on both motors.
-///
-/// param pwm_A  motor A PWM, -255 to 255
-/// param pwm_B  motor B PWM, -255 to 255
-void set_motor_currents(int pwm_A, int pwm_B)
-{
-  set_motor_pwm(pwm_A, motorA1Pin, motorA2Pin);
-  set_motor_pwm(pwm_B, motorB1Pin, motorB2Pin);
-
-  //Print a status message to the console.
-  Serial.print("Set motor A PWM = ");
-  Serial.print(pwm_A);
-  Serial.print("Set motor B PWM = ");
-  Serial.println(pwm_B);
-}
-
-/// Simple primitive for the motion sequence to set a speed and wait for an interval.
-///
-/// param pwm_A  motor A PWM, -255 to 255
-/// param pwm_B  motor B PWM, -255 to 255
-/// param duration delay in milliseconds
-void spin_and_wait(int pwm_A, int pwm_B, int duration) {
-  set_motor_currents(pwm_A, pwm_B);
-  delay(duration);
-}
-
 
 // source: https://forum.arduino.cc/t/reading-a-pwm-signal/603967/4
 byte GetPWM(byte pin) {
@@ -122,26 +72,8 @@ byte GetPWM(byte pin) {
   // pulseIn() returns zero on timeout
   if (highTime == 0 || lowTime == 0)
     return digitalRead(pin) ? 100 : 0;  // HIGH == 100%,  LOW = 0%
-//Serial.print("High Time: ");
-//Serial.print(highTime);
-//Serial.print(" Low Time: ");
-//Serial.println(lowTime);
-  rawpercent = ((100.0 * highTime) / ( highTime + lowTime));
-  scaledpercent = (rawpercent - 6.65) * 13.09; //scales based on test data
-  scaledpercent = constrain (scaledpercent, 0, 100); //Clamping rawpercent to only range between 0 and 100
 
-// Serial.println (scaledpercent);
-  return scaledpercent;
-  //return (100.0 * (float) highTime) / ((float) highTime + (float) lowTime);  // highTime as percentage of total cycle time
-}
-
-
-
-void TurnOffMotors(int motorA1, int motorA2, int motorB1, int motorB2) {
-  int allPins[] = {motorA1, motorA2, motorB1, motorB2};
-  for (int i=0; i<sizeof(allPins); i++) {
-    digitalWrite(allPins[i], LOW);
-  }
+  return (100 * highTime) / (highTime + lowTime);  // highTime as percentage of total cycle time
 }
 
 //Brainstorm junk
