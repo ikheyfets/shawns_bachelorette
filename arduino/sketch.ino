5828ee25560ee8a61f71ec95d4306a8019fe355d
Igor Kheyfets <44104569+ikheyfets@users.noreply.github.com>
2025-10-08 23:07:09 -0700

    Working prototype with steering (#1)
    * Update sketch.ino
    
    * Motor A works, Motor B should
    
    * Adding resolution to the getPWM function
    
    * Add initial steering logic to scale down the motor we are turning towards, based on how far the wheel is turned in that direction
    
    * Update direction change based on which speed is applied to which motor, also reversed positive logic
    
    * Converting tab characters to spaces
    
    * Attempt at adding zero radius no throttle turning
    
    * Re-adjust dead zones to zero the output when inside deadzone
    
    ---------
    
    Co-authored-by: Immunoglobin <71591845+Immunoglobin@users.noreply.github.com>
    Co-authored-by: Nathaniel Jefferson <nathaniel.jefferson@gmail.com>

--- arduino/sketch.ino
+++ arduino/sketch.ino
@@ -4,12 +4,14 @@ const int steeringInputPin = 2; // Connect the PWM signal from the receiver chan
 const int throttlingInputPin = 3; // Connect the PWM signal from the receiver channel 2 (throttling)
 
 // Defining motor A control inputs
-const int motorA1Pin = 5; //was 10 
+const int motorA1Pin = 7; //was 10 
 const int motorA2Pin = 6; //was 9
+const int motorAPwmPin = 5; // moved pins around because one of them needs to be PWM 
 
-// Defining motor B control inputs
-const int motorB1Pin = 9; //was 6
+// // Defining motor B control inputs
+const int motorB1Pin = 8; 
 const int motorB2Pin = 10; //was 5
+const int motorBPwmPin = 9;
 
 // PWM code source: https://lastminuteengineers.com/drv8833-arduino-tutorial/
 
@@ -20,10 +22,14 @@ void setup() {
 
   pinMode(motorA1Pin, OUTPUT);
   pinMode(motorA2Pin, OUTPUT);
+  pinMode(motorAPwmPin, OUTPUT);
+  
   pinMode(motorB1Pin, OUTPUT);
   pinMode(motorB2Pin, OUTPUT);
+  pinMode(motorBPwmPin, OUTPUT);
 
-  TurnOffMotors(motorA1Pin, motorA2Pin, motorB1Pin, motorB2Pin);
+
+  // TurnOffMotors(motorA1Pin, motorA2Pin, motorB1Pin, motorB2Pin);
 
   // Start serial communication at 9600 bps
   Serial.begin(9600);
@@ -33,9 +39,14 @@ void setup() {
 
 void loop() {
   // Serial.print("Steer Percentage = "); //100 is left, 0 is right
-  byte steer = GetPWM(steeringInputPin);
+  float steer = GetPWM(steeringInputPin);
   // Serial.print("Throttle Percentage = "); //100 is full forward, 0 is full reverse
-  byte throttle = GetPWM(throttlingInputPin);
+  float throttle = GetPWM(throttlingInputPin);
+   Serial.println("");
+  Serial.println("");
+  Serial.println("Throttle = ");
+  Serial.println(throttle);
+  Serial.println("");
   
   //  if both are zero, the transmitter is OFF, do nothing 
   if (steer == 0 && throttle == 0) {
@@ -43,105 +54,102 @@ void loop() {
     return; // Skip the remaining loop and move on to the next iteration
   }
 
- int ThrottlePWMOut = (((int) throttle - 50) * 5.1);
- set_motor_currents(ThrottlePWMOut,ThrottlePWMOut); //Math for PWM A
-  
-  // Ramp speed up.
-//  for (int i = 0; i < 11; i++) { //Initial condition = 0; While i<11, continue with loop; after every loop, add 1 to i
-//    spin_and_wait(25*i, 25*i, 500);
-//  }
+  //  6-10 is throttle back, 10-14 is throttle forward
+  int speedscalemax = 255;
+  float speed;
+  speed = speedscalemax * ((throttle - 50)/50);
+  Serial.println("Setting speed to:");
+  Serial.println(speed);
 
-  // Log the data
-  // Serial.print("steer PWM = ");
-  // Serial.print(steer);
-  // Serial.println();
 
-  // Serial.print("throttle PWM = ");
-  // Serial.print(throttle);
-  // Serial.println();
+  // Do Steering Calculations
 
-  // TODO: normalize the PWM data
+  // Set steering scaleing factor can be -1.0 to 1.0
+  float leftTurnScale = 1.0;
+  float rightTurnScale = 1.0;
 
-  // TODO: custom logic to drive two motors
+  // bound steer -50 to 50;
+  steer = steer - 50;
 
-  // Wait for a short duration
-  delay(10);
-}
+  //set a deadzone of 10%
+  if (abs(steer) > 10) {
 
-/// Set the current on a motor channel using PWM and directional logic.
-///
-/// param pwm    PWM duty cycle ranging from -255 full reverse to 255 full forward
-/// param IN1_PIN  pin number xIN1 for the given channel
-/// param IN2_PIN  pin number xIN2 for the given channel
-void set_motor_pwm(int pwm, int IN1_PIN, int IN2_PIN)
-{
-  if (pwm < 0) {  // reverse speeds
-    analogWrite(IN1_PIN, -pwm);
-    digitalWrite(IN2_PIN, LOW);
+    if (steer < 0) {  // Turn right
+      // Steer is -50 to 0 here
+      rightTurnScale = (steer + 25) / 25;
+    } else {  // Turn Left
+      // Steer is 0 to 50 here
+      leftTurnScale = (25 - steer) / 25;
+    }
 
-  } else { // stop or forward
-    digitalWrite(IN1_PIN, LOW);
-    analogWrite(IN2_PIN, pwm);
+    // Special case for no throttle turning (zero radius)
+    // Check that throttle is in the dead zone
+    // Only zero radius turn if turning more than half way in that direction
+    if (abs(speed) < 20 && abs(steer) > 25) {
+      // Set speed to be scaled down on the motors
+      speed = 255;
+      // If turning right, set our left motor to be the opposite direction and power
+      if (steer < 0)
+        leftTurnScale = -rightTurnScale;
+      // If turning left, set our right motor to be the opposite direction and power
+      else
+        rightTurnScale = -leftTurnScale;
+    }
+
   }
+
+  float leftSpeed = speed * leftTurnScale;
+  float rightSpeed = speed * rightTurnScale;
+
+  // set direction:
+  if (leftSpeed < 0) {
+    digitalWrite(motorA1Pin, LOW);
+    digitalWrite(motorA2Pin, HIGH);
+  } else {
+    digitalWrite(motorA1Pin, HIGH);
+    digitalWrite(motorA2Pin, LOW);
+  }
+  if (rightSpeed < 0) {
+    digitalWrite(motorB1Pin, LOW);
+    digitalWrite(motorB2Pin, HIGH);
+  } else {
+    digitalWrite(motorB1Pin, HIGH);
+    digitalWrite(motorB2Pin, LOW);
+  }
+
+  //  set duty cycle:
+  if ( abs(leftSpeed) < 20 )
+    leftSpeed = 0;
+  if ( abs(leftSpeed) < 20 )
+    rightSpeed = 0;
+  analogWrite(motorAPwmPin, abs(leftSpeed));
+  analogWrite(motorBPwmPin, abs(rightSpeed));
 }
-
-/// Set the current on both motors.
-///
-/// param pwm_A  motor A PWM, -255 to 255
-/// param pwm_B  motor B PWM, -255 to 255
-void set_motor_currents(int pwm_A, int pwm_B)
-{
-  set_motor_pwm(pwm_A, motorA1Pin, motorA2Pin);
-  set_motor_pwm(pwm_B, motorB1Pin, motorB2Pin);
-
-  //Print a status message to the console.
-  Serial.print("Set motor A PWM = ");
-  Serial.print(pwm_A);
-  Serial.print("Set motor B PWM = ");
-  Serial.println(pwm_B);
-}
-
-/// Simple primitive for the motion sequence to set a speed and wait for an interval.
-///
-/// param pwm_A  motor A PWM, -255 to 255
-/// param pwm_B  motor B PWM, -255 to 255
-/// param duration delay in milliseconds
-void spin_and_wait(int pwm_A, int pwm_B, int duration) {
-  set_motor_currents(pwm_A, pwm_B);
-  delay(duration);
-}
-
 
 // source: https://forum.arduino.cc/t/reading-a-pwm-signal/603967/4
 byte GetPWM(byte pin) {
   unsigned long highTime = pulseIn(pin, HIGH,50000UL);  // 50 millisecond timeout
   unsigned long lowTime = pulseIn(pin, LOW, 50000UL);  // 50 millisecond timeout
-  float rawpercent;
-  float scaledpercent; //function returns a byte anyway
+  float rawPercent;
+  float scaledPercent; //function returns a byte anyway
 
   // pulseIn() returns zero on timeout
   if (highTime == 0 || lowTime == 0)
     return digitalRead(pin) ? 100 : 0;  // HIGH == 100%,  LOW = 0%
-//Serial.print("High Time: ");
-//Serial.print(highTime);
-//Serial.print(" Low Time: ");
-//Serial.println(lowTime);
-  rawpercent = ((100.0 * highTime) / ( highTime + lowTime));
-  scaledpercent = (rawpercent - 6.65) * 13.09; //scales based on test data
-  scaledpercent = constrain (scaledpercent, 0, 100); //Clamping rawpercent to only range between 0 and 100
 
-// Serial.println (scaledpercent);
-  return scaledpercent;
-  //return (100.0 * (float) highTime) / ((float) highTime + (float) lowTime);  // highTime as percentage of total cycle time
-}
+  // Calculate the raw duty cycle percentage
+  rawPercent = ((100.0 * highTime) / ( highTime + lowTime ));
 
+  // Scale the 6 to 14 percentage to the full 100 percentage scale
+  // These scaling factors were determined by the range on the throttle signal
+  scaledPercent = ( rawPercent - 6.65 ) * 13.09;
 
+  // Clamp the scaled percentage to be between 0 and 100 for later scaling
+  scaledPercent = constrain ( scaledPercent, 0, 100 );
 
-void TurnOffMotors(int motorA1, int motorA2, int motorB1, int motorB2) {
-  int allPins[] = {motorA1, motorA2, motorB1, motorB2};
-  for (int i=0; i<sizeof(allPins); i++) {
-    digitalWrite(allPins[i], LOW);
-  }
+  // Return a value between 0 to 100, with based on the recieved radio input signal
+  return scaledPercent;
+  //return (100 * highTime) / (highTime + lowTime);  // highTime as percentage of total cycle time
 }
 
 //Brainstorm junk
