7427057ffab23bf68cd53fe2abeb9f4314dd8ea2
Shawn C <71591845+Immunoglobin@users.noreply.github.com>
2025-12-10 13:28:58 -0500

    Updated comments and posed questions to test

--- arduino/Tankbot_Control_Logic.ino
+++ arduino/Tankbot_Control_Logic.ino
@@ -65,14 +65,14 @@ void loop() {
 
   // Do Steering Calculations
 
-  // Set steering scaleing factor can be -1.0 to 1.0
+  // Set steering scaling factor to be -1.0 to 1.0
   float leftTurnScale = 1.0;
   float rightTurnScale = 1.0;
 
-  // bound steer -50 to 50;
+  // bound steer -50 to 50; Does this actually bould steer from -50 to 50?
   steer = steer - 50;
 
-  //set a deadzone of 10%
+  //set a deadzone of 10% Wouldn't this set a deadzone of 20%? Steer is -50 to 50, rather than -100 to 100
   if (abs(steer) > 10) {
 
     if (steer < 0) {  // Turn right
